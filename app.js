0b1c13e9e7345a983de5d8f625c88fbf46a81dd5
hanna <hanna.aspblad@iths.se>
2021-05-06 14:30:06 +0200

    error handler

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ require('dotenv').config()
 
 const app = express()
 const userRoutes = require('./routes/users')
+const errorHandler = require('./middleware/errorHandler')
 
 app.use( express.json() )
 
 app.use('/api/v1', userRoutes)
+
+app.use(errorHandler)
 
 
 

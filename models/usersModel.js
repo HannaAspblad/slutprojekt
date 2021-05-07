d9555a73aa08f43287dd3cda679de6d7d423a9c8
Viktor Okomdal <viktor_okomdal@hotmail.com>
2021-05-07 10:54:11 +0200

    auth middleware/ login in usermodel and controller

--- models/usersModel.js
+++ models/usersModel.js
@@ -3,6 +3,8 @@ const {DataTypes} = require('sequelize')
 const Messages = require('../models/messagesModel')
 const Task = require('../models/tasksModel')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { InvalidCredentials, Unauthorized } = require('../errors/errors.js')
 
 
 const User = db.define('User', {
@@ -38,6 +40,34 @@ User.hasMany(Task, {
 User.beforeCreate((user, options) =>{
   user.password = bcrypt.hashSync(user.password, 10)
 })
+
+User.authenticate = async (username, password) => {
+  const user = await User.findOne({where: {username}})
+  if(!user){ throw new InvalidCredentials() }
+  const passwordMatch = bcrypt.compareSync(password, user.password)
+  if(passwordMatch){
+    const payload = {id: user.id, username: user.username, role: user.role}
+    return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1w'})
+    
+  } else {
+    throw new InvalidCredentials()
+  }
+
+}
+
+User.validateToken = (token) => {
+  try{
+    return jwt.verify(token, process.env.JWT_SECRET)
+  } catch(error){
+    if( error instanceof jwt.TokenExpiredError){
+      throw new TokenExpiredError()
+    } else if ( error instanceof jwt.JsonWebTokenError){
+      throw new Unauthorized()
+    } else {
+      throw error
+    }
+  }
+}
 
 
 

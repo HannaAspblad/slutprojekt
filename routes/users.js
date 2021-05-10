7c2c28d05757f5e74d4226f0ce9355032c77ef79
Viktor Okomdal <viktor_okomdal@hotmail.com>
2021-05-10 14:58:09 +0200

    /me endpoint almost done

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,13 @@ const {Router} = require('express')
 
 const router = new Router()
 const userController = require('../controllers/usersController')
+const Auth = require('../middleware/auth')
+
 
 
 router.post('/users', userController.createUser)
 router.post('/authenticate', userController.login)
+router.get('/me',Auth.client, userController.me)
 
 
 module.exports = router

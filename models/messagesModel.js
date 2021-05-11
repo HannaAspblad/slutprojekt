1f37101908e29f3b69211ef4d4a8e5f99a89f739
Malin S <malin.soderstrom@iths.se>
2021-05-11 13:14:43 +0200

    finished message flow with errorhandling

--- models/messagesModel.js
+++ models/messagesModel.js
@@ -1,6 +1,5 @@
 const db = require('../database/connection.js')
 const {DataTypes} = require('sequelize')
-//const Task = require('./tasksModel')
 
 
 const Message = db.define('Message', {
@@ -9,4 +8,31 @@ const Message = db.define('Message', {
     allowNull:false
   }
 })
+
+Message.findAllSorted = async (page, task) => {
+  try{
+    const messages = await Message.findAll({where: { TaskId: task }, offset: (page -1) * 5, limit: 5, order: ['createdAt']})
+    return messages
+  } catch(err){
+    throw err
+  }
+}
+
+Message.matchTask = async (message, task) => {
+  if(await Message.findOne({ where: {id: message, TaskId: task} }) ) {
+    return true
+  } else{
+    return false
+  }
+}
+
+Message.deleteMessage = async (message, task) => {
+  await Message.destroy({ where: {id: message, TaskId: task} })
+}  
+
+Message.createMessage = async (text, task) => {
+  return await Message.create({ text: text, TaskId: task })
+}
+
+
 module.exports = Message
--- routes/messages.js
+++ routes/messages.js
@@ -2,7 +2,7 @@ const {Router} = require("express")
 const router = new Router()
 const MessageController = require('../controllers/messagesController')
 
-router.get('/tasks/:id/messages', MessageController.getAll)
+router.get('/tasks/:id/messages/:page', MessageController.getAll)
 router.post('/tasks/:id/messages', MessageController.create)
 router.delete('/tasks/:id/messages/:message', MessageController.delete)
 

acbd18299d519cb876d17da5c37ce9ce3ebe505b
Malin S <malin.soderstrom@iths.se>
2021-05-12 10:56:34 +0200

    renamed variable in auth

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,10 +4,11 @@ const router = new Router()
 const tasksController = require("../controllers/tasksController")
 const Auth = require("../middleware/auth")
 
+
 router.get("/tasks", tasksController.getTasks)
+router.post("/tasks", tasksController.createTask)
 
 router.get("/tasks/:id", Auth.workerClientAccess, tasksController.getTaskById)
-router.post("/tasks", tasksController.createTask)
 router.patch("/tasks/:id", tasksController.editTaskById)
 router.delete("/tasks/:id", tasksController.deleteTaskById)
 
